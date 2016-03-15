b3a4d5b00f9fde9b87f397df1c1354cc25b3eb7c
black2279 <black.andrighetto94@gmail.com>
2016-03-15 18:59:51 +0100

    Aggiunto file batch e note per compilare Glossario su Windows

---
+++ Documents/RP/ExternalDocuments/Glossario/AutomaticGlossaryGenerator.bat
@@ -0,0 +1,11 @@
+perl glossario.pl
+pdflatex Glossario.tex
+makeglossaries Glossario
+pdflatex Glossario
+del Glossario.aux
+del Glossario.glo
+del Glossario.glg
+del Glossario.gls
+del Glossario.ist
+del Glossario.log
+del Glossario.out
